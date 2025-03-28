7f08e74915cc8fe2eca8daf85eee5c3a2d55d682
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-03-28 20:25:29 +0500

    code

--- 01DartLectures/Lecture04ConditionalStatement_switchcase.dart
+++ 01DartLectures/Lecture04ConditionalStatement_switchcase.dart
@@ -1,40 +1,44 @@
+// Lecture 04: Switch Statement in Dart
+// Instructor: Syed M Arsalan Shah
+
 void main() {
+  // Vowel Checker using Switch Statement
   String vowels = 'z';
 
   switch (vowels) {
     case 'A':
     case 'a':
       {
-        print("Its a vowel $vowels");
+        print("It's a vowel: $vowels");
         break;
       }
     case 'E':
     case 'e':
       {
-        print("Its a vowel $vowels");
+        print("It's a vowel: $vowels");
         break;
       }
     case 'I':
     case 'i':
       {
-        print("Its a vowel $vowels");
+        print("It's a vowel: $vowels");
         break;
       }
     case 'O':
     case 'o':
       {
-        print("Its a vowel $vowels");
+        print("It's a vowel: $vowels");
         break;
       }
     case 'U':
     case 'u':
       {
-        print("Its a vowel $vowels");
+        print("It's a vowel: $vowels");
         break;
       }
     default:
       {
-        print("its not a vowels");
+        print("It's not a vowel: $vowels");
       }
   }
 }
