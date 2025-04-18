ac5090e1bf3a41f23e88180f43e551671bb9176e
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-04-18 11:23:52 +0500

    Code updated

---
+++ 02FlutterLectures/lecture03buttonandnavigations/lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => About(),
+                  ));
+            },
+            child: Text("Next page")),
+      ),
+    );
+  }
+}
+
+class About extends StatefulWidget {
+  const About({super.key});
+
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: OutlinedButton(
+            onPressed: () {
+              // Navigator.pop(context);
+
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Contact()));
+            },
+            child: Text("Previous ")),
+      ),
+    );
+  }
+}
+
+class Contact extends StatefulWidget {
+  const Contact({super.key});
+
+  @override
+  State<Contact> createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TextButton(
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => Home()));
+          },
+          child: Text("Home Screen")),
+    );
+  }
+}
